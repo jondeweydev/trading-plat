df0fca5a9aad45e0881143fef2e51a57a39696fd
Jon Dewey <jon@169-231-60-207.wireless.ucsb.edu>
2023-10-30 22:44:38 -0700

    added new symbol check

--- script/populate_db.py
+++ script/populate_db.py
@@ -19,8 +19,21 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(script_dir, "..", "db", "app.db")
 # connect to DB
 connection = sqlite3.connect(db_path)
+
+# set querying type to row object
+connection.row_factory = sqlite3.Row
 # setup cursor
 cursor = connection.cursor()
+
+# query select rows
+cursor.execute("""
+    SELECT symbol, company FROM stock
+""")
+
+# grab all rows from cursor
+rows = cursor.fetchall()
+
+symbols = [row['symbol'] for row in rows]
 
 # grab alpaca data
 assets = api.get_all_assets()
@@ -28,8 +41,10 @@ assets = api.get_all_assets()
 # add only active and tradable assets to local db
 for asset in assets:
     try:
-        if asset.status == "active" and asset.tradable:
-            cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
+        if asset.status == "active" and asset.tradable and asset.symbol not in symbols:
+            print(f"Added a new stock {asset.symbol} {asset.name}")
+            cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", 
+                           (asset.symbol, asset.name))
     except Exception as e:
         print(e)
 
